04913d2ee617b0a4463fac3ca651c278d317ae22
Rojan <devrojantimbas@gmail.com>
2023-05-22 20:37:52 +0800

    create an api for getting all user

---
+++ src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
+import { PrismaService } from 'src/prisma.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get('/all')
+  @UseGuards(JwtAuthGuard)
+  async getAll() {
+    return await this.prisma.user.findMany({
+      select: { email: true, firstName: true, lastName: true, status: true },
+    });
+  }
+}
