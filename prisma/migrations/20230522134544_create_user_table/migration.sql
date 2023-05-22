e52b101ca1c4094e78b01ee0d294c90f35bc6eaa
Rojan <devrojantimbas@gmail.com>
2023-05-22 23:00:38 +0800

    create migration for user table

---
+++ prisma/migrations/20230522134544_create_user_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "status" BOOLEAN NOT NULL DEFAULT false,
+    "activationToken" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
