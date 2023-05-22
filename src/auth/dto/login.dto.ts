c1f8b6d83add560b64f159588250db752494c5f4
Rojan <devrojantimbas@gmail.com>
2023-05-22 23:07:08 +0800

    add more validation

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, Min } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
@@ -6,6 +12,15 @@ export class LoginDto {
   email: string;
 
   @IsNotEmpty()
-  @Min(6)
+  @MinLength(6, {
+    message: 'Password must be longer than or equal to 6 characters',
+  })
+  @MaxLength(20, {
+    message: 'Password must be shorter than or equal to 20 characters',
+  })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain upper case and lower case letters and number or special character ',
+  })
   password: string;
 }
