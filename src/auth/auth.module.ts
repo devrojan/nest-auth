a36344cdcf5f4d5621d12dd89de27fc702713e96
Rojan <devrojantimbas@gmail.com>
2023-05-22 23:02:29 +0800

    add prisma service as providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' }
-    })
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
   controllers: [AuthController],
   // add the jwt strategy
-  providers: [AuthService, JwtStrategy]
+  providers: [AuthService, PrismaService, JwtStrategy],
 })
-export class AuthModule { }
+export class AuthModule {}
